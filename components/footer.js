3209fb3ae38a64d1119bc117dde3f9589450b355
Moses Dare Aleriwon <aleriwonmoses@gmail.com>
2024-11-19 12:02:11 +0100

    part UI design for homepage

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function Footer (){
     return(
-        <footer className="flex flex-col lg:flex-row lg:justify-between gap-4 bg-white py-4 md:py-6  px-2 md:px-8 lg:px-16 lg:px-16">
+        <footer className="flex flex-col lg:flex-row lg:justify-between gap-4 bg-white py-4 md:py-6  px-2 md:px-8 lg:px-16">
             <ul className="flex flex-wrap gap-4 md:gap-6">
                 <li className="text-xs text-gray-600"><Link href="#">Locations</Link></li>
                 <li className="text-xs text-gray-600"><Link href="#">Sitemap</Link></li>
